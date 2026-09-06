1a302cc1300717733d0fcab6f2219e47d8dc0bc6
Ankit Chobdar <ankitchobdar@gmail.com>
2026-09-06 08:35:01 +0530

    Added Put api back to controller

--- src/main/java/org/project/claimsmgmt/controller/ClaimController.java
+++ src/main/java/org/project/claimsmgmt/controller/ClaimController.java
@@ -34,13 +34,13 @@ public class ClaimController {
         return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
+    //Adding back put api
     @PutMapping("/{id}")
     public ResponseEntity<Claim> update(@PathVariable String id, @RequestBody Claim claim) {
         return claimService.update(id, claim)
                 .map(c -> ResponseEntity.ok(claim))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
-
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id) {
